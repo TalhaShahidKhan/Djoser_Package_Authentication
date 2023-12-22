01f05a5e826c37597bf364a3fa5ee59559168d84
talhakhan54 <talhashahidkhan49@gmail.com>
2023-12-22 09:10:28 +0600

    Admin panale bug fix

--- users/models.py
+++ users/models.py
@@ -1,34 +1,25 @@
 from django.db import models
+from typing import Any
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 # Custom User Manager
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
-
-    def create_user(self, email, password=None, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+    def create_user(self, username: str, password=None, **extra_fields: Any) -> Any:
+        if not username:
+            raise ValueError("Username is required.")
+        
+        user = self.model(username = username, password=password,**extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save(using= self.db)
         return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        """
-        Creates and saves a superuser with the given email and password.
-        """
-        user = self.create_user(email, password, **extra_fields)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-    
-
+        
+    def create_superuser(self, username: str, password=None, **extra_fields: Any) -> Any:
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_active',True)
+        extra_fields.setdefault('is_superuser',True)
+        return self.create_user(username, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
