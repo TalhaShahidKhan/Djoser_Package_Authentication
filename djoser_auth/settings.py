d280a515ff6e4d7864e304fdbf2a36488382a0f0
talhakhan54 <talhashahidkhan49@gmail.com>
2023-12-22 08:03:52 +0600

    Permissions Class

--- djoser_auth/settings.py
+++ djoser_auth/settings.py
@@ -134,6 +134,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # REST FRAMEWORK 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticated'
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
