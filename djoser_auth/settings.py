86c94828b89a22f821d320ec1365a31d6559ba41
talhakhan54 <talhashahidkhan49@gmail.com>
2023-12-22 08:16:33 +0600

    Custom User Model and Migrations

--- djoser_auth/settings.py
+++ djoser_auth/settings.py
@@ -118,6 +118,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+# CustomUser 
+AUTH_USER_MODEL = 'users.CustomUser'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(email, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    
+
+
+
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
+    REQUIRED_FIELDS= []
+    def __str__(self) -> str:
+        return self.username
